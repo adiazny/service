b345502465305a2ec83c81529efbc71f48dc3e1e
Alan Diaz <adiazny@gmail.com>
2021-12-12 18:04:53 -0500

    Add sending responses

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -3,9 +3,9 @@ package testgrp
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 
+	"github.com/adiazny/service/foundations/web"
 	"go.uber.org/zap"
 )
 
@@ -26,6 +26,6 @@ func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Reque
 
 	h.Log.Infow("test", "statusCode", statusCode, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
 
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, statusCode)
 
 }
